f709d5bfa80a1506a09f5fadb9a9318d159e84bd
aloadeuado <79708828+aloadeuado@users.noreply.github.com>
2023-06-05 09:42:11 -0500

    add carrusel

---
+++ test_meli/test_meli/App/Scene/DetailProduct/View/Reusable/CarouselImage/Cell/CarouselImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CarouselImageCollectionViewCell.swift
+//  test_meli
+//
+//  Created by Pedro Alonso Daza B on 5/06/23.
+//
+
+import UIKit
+
+class CarouselImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
